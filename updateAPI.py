ad014e3b5048dbc4332051e5381e7c25e81f94ea
gw97 <41743460+gw97@users.noreply.github.com>
2018-08-28 14:35:35 +0100

    Add files via upload
    NEw

---
+++ updateAPI.py
@@ -0,0 +1,23 @@
+#script to install latest version from API repository from GitHub
+#script to install latest version from API repository from GitHub
+
+import os
+import time
+os.system
+#deletes old programs
+
+os.system("rm TGpub.py")
+os.system("rm VOCGpub.py")
+
+#os.system("rm updateAPI.py")
+#os.system("rm README.md")
+#clones repository
+os.system("git clone https://github.com/Chriisbrown/API.git")
+time.sleep(20)
+#moves files out of API file to where update.py stored
+os.system("mv /home/pi/Temperature/API/TGpub.py /home/pi/Temperature/TGpub.py")
+os.system("mv /home/pi/Temperature/API/VOCGpub.py /home/pi/Temperature/VOCGpub.py")
+#os.system("mv /Users/george/Desktop/Anaphite/API/VOCGpub.py /Users/george/Desktop/Anaphite/VOCpub.py")
+
+#removes the API directory now relavent contents have been removed
+os.system("rm -r -f /home/pi/Temperature/API")
